f776ab792bfa129d8185aa331b2aeb3454fea4b4
Artur Alves <arturacb@gmail.com>
2025-04-08 03:21:17 -0300

    Server and clientimplemented

---
+++ src/client.c
@@ -0,0 +1,249 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+
+#define SERVER_PORT "6666" 
+#define SERVER_ADDR "localhost" 
+#define BUF_SIZE 1024 
+#define MAX_ARGS 10
+
+// Helper function to split client input 
+int split_args(char *input, char **args, int max_args) {
+    int argc = 0;
+    char *p = input;
+
+    while (*p && argc < max_args) {
+        while (*p == ' ') p++;  
+        if (*p == 0) break;
+        if (*p == '"') {
+            p++; 
+            args[argc] = p;
+            while (*p && *p != '"') p++;
+            if (*p == '"') {
+                *p = 0;  
+                p++;
+            }
+            argc++;
+        } else {
+            args[argc] = p;
+            while (*p && *p != ' ') p++;
+            if (*p == ' ') {
+                *p = 0;
+                p++;
+            }
+            argc++;
+        }
+    } 
+
+    return argc;
+}
+
+/*
+ * read_response reads data from sockfd into resp until the termination marker ("END_OF_RESPONSE\n")
+ * is found. It uses a static leftover buffer to store any extra data beyond the marker.
+ */
+
+// helper function to read the data from sockfd into resp until receiving END_OF_RESPONSE
+int read_response(int sockfd, char *resp, int resp_size) {
+    static char leftover[BUF_SIZE] = "";
+    int total = 0;
+    
+    if (strlen(leftover) > 0) {
+        strncpy(resp, leftover, resp_size - 1);
+        resp[resp_size - 1] = '\0';
+        total = strlen(resp);
+        leftover[0] = '\0';
+    } else {
+        resp[0] = '\0';
+        total = 0;
+    }
+    
+    while (1) {
+        char *marker = strstr(resp, "END_OF_RESPONSE\n");
+        if (marker) {
+            *marker = '\0';
+            marker += strlen("END_OF_RESPONSE\n");
+            if (*marker != '\0') {
+                strncpy(leftover, marker, BUF_SIZE - 1);
+                leftover[BUF_SIZE - 1] = '\0';
+            }
+            break;
+        }
+        // read data until receive marker
+        int bytes = recv(sockfd, resp + total, resp_size - total - 1, 0);
+        if (bytes <= 0) {
+            break;
+        }
+        total += bytes;
+        resp[total] = '\0';
+    }
+    return total;
+}
+
+void printHelpInfo(){
+    printf("Available commands:\n");
+    printf("  addMovie title genre director debutDate -> Adds movie to database\nGenres must be in format 'genre1,genre2,...', and data must be in format YYYY-MM-DD\n");
+    printf("  addGenre id genre -> adds genre to movie with specified id if it exists\n");
+    printf("  remove id -> remove movie with specified id\n");
+    printf("  list (argument) (optional)\n");
+    printf("    arguments:\n");
+    printf("      -all -> List entire database\n");
+    printf("      -movies -> List all movies alongside their IDs\n");
+    printf("      -i id -> List information about a specific movie, must give a INT as ID\n");
+    printf("      -g genre -> List all movies of specified genre\n");
+    printf("  -h, help -> Show help message\n");
+    printf("  exit -> Exit program\n");
+}
+
+int main() {
+    // Networking variables
+    struct addrinfo hints, *res;
+    int sockfd;
+    int status;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;     
+    hints.ai_socktype = SOCK_STREAM; 
+
+    status = getaddrinfo(SERVER_ADDR, SERVER_PORT, &hints, &res);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        return 1;
+    }
+
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sockfd == -1) {
+        fprintf(stderr, "socket error: %s\n", strerror(errno));
+        freeaddrinfo(res);
+        return 1;
+    }
+
+    status = connect(sockfd, res->ai_addr, res->ai_addrlen);
+    if (status == -1) {
+        fprintf(stderr, "connect error: %s\n", strerror(errno));
+        close(sockfd);
+        freeaddrinfo(res);
+        return 1;
+    }
+
+    // Connected, now the user can request infos
+    printf("Welcome to MovieDB, you are now connected to the land of movies information!\nFor exiting type 'exit' \nType -h if you need help :)\n\n");
+
+    char IObuffer[BUF_SIZE];
+    static char request_buf[BUF_SIZE]; 
+    char *request;
+    char *args[MAX_ARGS];
+
+    while (1){
+        memset(IObuffer, 0, BUF_SIZE);
+        printf(">> ");
+        if (fgets(IObuffer, BUF_SIZE, stdin) == NULL) {
+            break;
+        }
+        IObuffer[strcspn(IObuffer, "\n")] = 0;  
+
+        int argc = split_args(IObuffer, args, MAX_ARGS);
+        if (argc == 0) continue;
+
+        char *command = args[0];
+        request = request_buf; 
+
+        if (strcmp(command, "exit") == 0) {
+            break;
+        } 
+        else if (strcmp(command, "addMovie") == 0) {
+            if (argc != 5) {
+                printf("Usage: addMovie title genres director debutDate\n");
+                continue;
+            }
+            snprintf(request_buf, BUF_SIZE, "ADDMOVIE \"%s\" \"%s\" \"%s\" \"%s\"", args[1], args[2], args[3], args[4]);
+            // printf("Request prepared: %s\n", request);
+        }
+        else if (strcmp(command, "addGenre") == 0) {
+            if (argc != 3) {
+                printf("Usage: addGenre id genre\n");
+                continue;
+            }
+            snprintf(request_buf, BUF_SIZE, "ADDGENRE %s %s", args[1], args[2]);
+            // printf("Request prepared: %s\n", request);
+        }
+        else if (strcmp(command, "remove") == 0) {
+            if (argc != 2) {
+                printf("Usage: remove id\n");
+                continue;
+            }
+            snprintf(request_buf, BUF_SIZE, "REMOVE %s", args[1]);
+            // printf("Request prepared: %s\n", request);
+        }
+        else if (strcmp(command, "list") == 0) {
+            if (argc == 1) {
+                strcpy(request_buf, "LIST ALL");
+                // printf("Request prepared: %s\n", request);
+            }
+            else if (argc == 2) {
+                if (strcmp(args[1], "-all") == 0) {
+                    strcpy(request_buf, "LIST ALL");
+                    // printf("Request prepared: %s\n", request);
+                }
+                else if (strcmp(args[1], "-movies") == 0) {
+                    strcpy(request_buf, "LIST MOVIES");
+                    // printf("Request prepared: %s\n", request);
+                }
+                else {
+                    printf("Invalid list option.\n");
+                    continue;
+                }
+            }
+            else if (argc == 3) {
+                if (strcmp(args[1], "-i") == 0) {
+                    snprintf(request_buf, BUF_SIZE, "LIST ID %s", args[2]);
+                    // printf("Request prepared: %s\n", request);
+                }
+                else if (strcmp(args[1], "-g") == 0) {
+                    snprintf(request_buf, BUF_SIZE, "LIST GENRE %s", args[2]);
+                    // printf("Request prepared: %s\n", request);
+                }
+                else {
+                    printf("Invalid list option.\n");
+                    continue;
+                }
+            }
+            else {
+                printf("Invalid list command\n");
+                continue;
+            }
+        }
+        else if (strcmp(command, "-h") == 0 || strcmp(command, "help") == 0) {
+            printHelpInfo();
+            continue;
+        }
+        else {
+            printf("Invalid command\n");
+            continue;
+        }
+    
+        // Send request 
+        ssize_t bytes_sent = send(sockfd, request, strlen(request), 0);
+        if (bytes_sent == -1) {
+            fprintf(stderr, "send error: %s\n", strerror(errno));
+            break;
+        }
+        // printf("request sent!\n");
+        
+        // Read the response until receiveng the termination marker
+        char response[BUF_SIZE];
+        memset(response, 0, BUF_SIZE);
+        read_response(sockfd, response, BUF_SIZE);
+        printf("%s\n", response); 
+    }
+
+    // Cleanup
+    close(sockfd);
+    freeaddrinfo(res);
+    return 0;
+}
--- src/server.c
+++ src/server.c
@@ -1,44 +1,220 @@
 #include<stdio.h>
 #include<string.h>
+#include<errno.h>
+#include<stdlib.h>
 #include<unistd.h>
+#include<netdb.h>
 #include<sys/types.h>
 #include<sys/socket.h>
-#include<netdb.h>
-#include<sqlite3.h>
+#include <ctype.h>
 
-#define MYPORT "9666"
-#define BACKLOG 10
+#include<sqlite3.h>
+#include<pthread.h>
+
+#define MYPORT "6666"
+#define BACKLOG 20
+#define BUF_SIZE 1024
+#define MAX_ARGS 10
+
+sqlite3 *db;
+
+// Helper function to split the client request
+int split_request(char *buffer, char **args, int max_args) {
+    int argc = 0;          
+    char *p = buffer;     
+    int in_quote = 0;   
+
+    while (*p && argc < max_args) {
+        while (isspace((unsigned char)*p)) p++;
+        if (*p == 0) break; 
+        args[argc] = p;
+        argc++;
+        while (*p) {
+            if (*p == '"') {
+                in_quote = !in_quote;  
+                memmove(p, p + 1, strlen(p + 1) + 1);
+                continue;  
+            } else if (in_quote) {
+                p++;
+            } else if (isspace((unsigned char)*p)) {
+                *p = 0;  
+                p++;
+                break;
+            } else {
+                p++;
+            }
+        }
+    }
+
+    return argc;
+}
+
 
 // Callback for db
-static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+static int callback(void *data, int argc, char **argv, char **azColName){
+    int client_fd = *(int *)data;
+    char buf[BUF_SIZE];
     for(int i=0; i<argc; i++){
-       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+       snprintf(buf, sizeof buf, "%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+       send(client_fd, buf, strlen(buf), 0);
     }
-    printf("\n");
+    send(client_fd, "\n", 1, 0);
     return 0;
 }
 
 // Add movie to the database
-int addMovie(sqlite3 *db, const char *sql){
-    char *errMsg = 0;
+int addMovie(sqlite3 *db, char *title, char *genres, char *director, char *debutDate, char **response){
+    sqlite3_stmt *stmt;
     int rc;
-    //sql = "INSERT INTO movies (title, genres, director, debutDate)"
-    //      "VALUES ('Get Out', 'Horror,Comedy', 'Jordan Peele', '2017-05-18');";
-    rc = sqlite3_exec(db, sql, callback, 0, &errMsg);
-    if(rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error: %s\n", errMsg);
-        sqlite3_free(errMsg);
-    } else {printf("Movie added!\n");}
+    const char *sql = "INSERT INTO movies (title, genres, director, debutDate) VALUES (?, ?, ?, ?);";
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        *response = malloc(BUF_SIZE);
+        snprintf(*response, BUF_SIZE, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+        return rc;
+    } 
+
+    // printf("title is %s, genres is %s, director is %s, and debutDate is %s\n", title, genres, director, debutDate);
+
+    sqlite3_bind_text(stmt, 1, title, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 2, genres, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 3, director, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 4, debutDate, -1, SQLITE_TRANSIENT);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+        sqlite3_finalize(stmt);  
+        *response = malloc(BUF_SIZE);
+        snprintf(*response, BUF_SIZE, "Failed to add movie: %s\n", sqlite3_errmsg(db));
+        return rc;
+    }
+    // char *expanded_sql = sqlite3_expanded_sql(stmt);
+    // if (expanded_sql) {
+    //     printf("Expanded SQL: %s\n", expanded_sql);
+    //     sqlite3_free(expanded_sql);  
+    // } else {
+    //     printf("Failed to expand SQL statement.\n");
+    // }
+
+    sqlite3_finalize(stmt);
+
+    *response = malloc(BUF_SIZE);
+    snprintf(*response, BUF_SIZE, "Movie added successfully!\n");
+
+    return rc;
+}
+
+// Add genre to movie with specified id, if the movie exists
+int addGenreToMovie(sqlite3 *db, int id, const char *genre, char **response){
+    sqlite3_stmt *stmt;
+    int rc;
+    char sql[BUF_SIZE];
+
+    // Verify if the movie exists
+    snprintf(sql, sizeof(sql), "SELECT COUNT(*) FROM movies WHERE id = %d;", id);
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        *response = malloc(BUF_SIZE);
+        snprintf(*response, BUF_SIZE, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+        return rc;
+    }
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_ROW) {
+        *response = malloc(BUF_SIZE);
+        snprintf(*response, BUF_SIZE, "Error checking movie existence: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(stmt);
+        return rc;
+    }
+    int count = sqlite3_column_int(stmt, 0);
+    sqlite3_finalize(stmt);
+
+    if (count == 0) {
+        *response = malloc(BUF_SIZE);
+        snprintf(*response, BUF_SIZE, "Movie not found: %s\n", sqlite3_errmsg(db));
+        return rc;
+    }
+
+    // Add the genre to the genres field
+    snprintf(sql, sizeof(sql),
+             "UPDATE movies SET genres = "
+             "CASE WHEN genres IS NULL OR genres = '' THEN ? "
+             "ELSE genres || ',' || ? END "
+             "WHERE id = %d;",
+             id);
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        *response = malloc(BUF_SIZE);
+        snprintf(*response, BUF_SIZE, "Error preparing stmt: %s\n", sqlite3_errmsg(db));
+        return rc;
+    }
+
+    sqlite3_bind_text(stmt, 1, genre, -1, SQLITE_TRANSIENT);
+    sqlite3_bind_text(stmt, 2, genre, -1, SQLITE_TRANSIENT);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+        *response = malloc(BUF_SIZE);
+        snprintf(*response, BUF_SIZE, "Some error occured: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(stmt);
+        return rc;
+    }
+
+    sqlite3_finalize(stmt);
+
+    *response = malloc(BUF_SIZE);
+    snprintf(*response, BUF_SIZE, "Genre added sucesfully!\n");
+
+    return rc;
+}
+
+// Removes movie with specified id
+int removeMovie(sqlite3 *db, int id, char **response){
+    sqlite3_stmt *stmt;
+    int rc;
+    const char *sql = "DELETE FROM movies WHERE id = ?;";
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        *response = malloc(BUF_SIZE);
+        snprintf(*response, BUF_SIZE, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+        return rc;
+    }
+
+    sqlite3_bind_int(stmt, 1, id);
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+        sqlite3_finalize(stmt);
+        *response = malloc(BUF_SIZE);
+        snprintf(*response, BUF_SIZE, "Failed to remove movie: %s\n", sqlite3_errmsg(db));
+        return rc;
+    }
+
+    // char *expanded_sql = sqlite3_expanded_sql(stmt);
+    // if (expanded_sql) {
+    //     printf("Expanded SQL: %s\n", expanded_sql);
+    //     sqlite3_free(expanded_sql);
+    // } else {
+    //     printf("Failed to expand SQL statement.\n");
+    // }
+
+    sqlite3_finalize(stmt);
+
+    *response = malloc(BUF_SIZE);
+    snprintf(*response, BUF_SIZE, "Movie removed successfully!\n");
 
     return rc;
 }
 
 // print entire database
-int printDatabase(sqlite3 *db){
+int listAll(sqlite3 *db, int fd){
     char *errMsg = 0;
     int rc;
     const char *sql = "SELECT * FROM movies;";
-    rc = sqlite3_exec(db, sql, callback, 0, &errMsg);
+
+    rc = sqlite3_exec(db, sql, callback, &fd, &errMsg);
     if (rc != SQLITE_OK) {
         fprintf(stderr, "SQL error: %s\n", errMsg);
         sqlite3_free(errMsg);
@@ -47,21 +223,164 @@ int printDatabase(sqlite3 *db){
     return rc;
 }
 
+// list entire database
+int listAllMovies(sqlite3 *db, int fd){
+    char *errMsg = 0;
+    int rc;
+    const char *sql = "SELECT id, title FROM movies;";
+
+    rc = sqlite3_exec(db, sql, callback, &fd, &errMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", errMsg);
+        sqlite3_free(errMsg);
+    }
+
+    return rc;
+}
+
+// list all movies alongside their ids
+int listMovie(sqlite3 *db, int id, int fd){
+    char *errMsg = 0; 
+    int rc;            
+    char sql[BUF_SIZE];    
+
+    snprintf(sql, sizeof(sql), "SELECT * FROM movies WHERE id = %d;", id);
+
+    rc = sqlite3_exec(db, sql, callback, &fd, &errMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", errMsg);
+        sqlite3_free(errMsg);
+        char *msg = "Error executing query\n";
+        send(fd, msg, strlen(msg), 0);
+        return rc;
+    }
+
+    return rc;
+}
+
+// list all movies with the specified genre
+int listMoviesByGenre(sqlite3 *db, const char *genre, int fd){
+    char *errMsg = 0;
+    int rc;
+    char sql[BUF_SIZE];  
+    char *lower_genre; 
+
+    // Convert genre to lowercase for better matching
+    size_t genre_len = strlen(genre);
+    lower_genre = malloc(genre_len + 1);
+    if (!lower_genre) {
+        char *msg = "Error: Memory allocation failed\n";
+        send(fd, msg, strlen(msg), 0);
+        return SQLITE_NOMEM;
+    }
+    for (size_t i = 0; i < genre_len; i++) {
+        lower_genre[i] = tolower((unsigned char)genre[i]);
+    }
+    lower_genre[genre_len] = '\0';
+
+    // Select the movies with specified genre
+    snprintf(sql, sizeof(sql),
+             "SELECT * FROM movies WHERE LOWER(',' || genres || ',') LIKE '%%,%s,%%';",
+             lower_genre);
+
+    rc = sqlite3_exec(db, sql, callback, &fd, &errMsg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", errMsg);
+        sqlite3_free(errMsg);
+        char *msg = "Error executing query\n";
+        send(fd, msg, strlen(msg), 0);
+        free(lower_genre);
+        return rc;
+    }
+
+    free(lower_genre);
+    return rc;
+}
+
+// function that is executed in each thread
+void *client_handler(void *arg) {
+    int client_fd = *(int *)arg;
+    free(arg);
+    char buffer[BUF_SIZE];
+    int bytes_received;
+
+    // loop for commands
+    while (1) {
+        memset(buffer, 0, BUF_SIZE);
+        bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received <= 0) {
+            printf("error ocurred in recv\n");
+            break;
+        }
+        buffer[bytes_received] = '\0';
+
+        // split the request
+        char *args[10];
+        int argc = split_request(buffer, args, MAX_ARGS);
+        if (argc <= 0)
+            continue;
+
+        // printf("command %s with %d args: ", args[0], argc);
+        // for (int i = 1; i < argc; i++) {
+        //     printf("arg[%d] is %s ", i, args[i]);
+        // }
+        // printf("\n");
+
+        // add and remove functions have a response argument for printing in the client
+        // if the operation was sucess
+        if (strcmp(args[0], "ADDMOVIE") == 0 && argc == 5) {
+            char *response;
+            addMovie(db, args[1], args[2], args[3], args[4], &response);
+            send(client_fd, response, strlen(response), 0);
+            free(response); 
+        } else if (strcmp(args[0], "ADDGENRE") == 0 && argc == 3) {
+            char *response;
+            addGenreToMovie(db, atoi(args[1]), args[2], &response);
+            send(client_fd, response, strlen(response), 0);
+            free(response); 
+        } else if (strcmp(args[0], "REMOVE") == 0 && argc == 2) {
+            char *response;
+            removeMovie(db, atoi(args[1]), &response);
+            send(client_fd, response, strlen(response), 0);
+            free(response);
+        } else if (strcmp(args[0], "LIST") == 0 ) {
+            if (strcmp(args[1], "ALL" ) == 0 && argc == 2){
+                listAll(db, client_fd);
+            } else if (strcmp(args[1], "MOVIES") == 0 && argc == 2){
+                listAllMovies(db, client_fd);
+            } else if (strcmp(args[1], "ID") == 0 && argc == 3) {
+                listMovie(db, atoi(args[2]), client_fd);
+            } else if (strcmp(args[1], "GENRE") == 0 && argc == 3) {
+                listMoviesByGenre(db, args[2], client_fd);
+            }
+        } else {
+            send(client_fd, "Something strange happened\n", 27, 0);
+        }
+        // marks end of response
+        send(client_fd, "\nEND_OF_RESPONSE\n", strlen("\nEND_OF_RESPONSE\n"), 0);
+    }
+    close(client_fd);
+    // printf("Conection close with client :/\n");
+    return NULL;
+}
+
 
 int main(){
     // Database variables
-    sqlite3 *db;
     char *errMsg = 0;
     int rc;
     const char *sql;
+
     // Networking variables
     struct sockaddr_storage their_addr;
     socklen_t addr_size;
     struct addrinfo hints, *res;
     int sockfd, new_fd;
+    int status;
 
+    // Open DB and initialize table
     rc = sqlite3_open("test.db", &db);
-    if (rc) {
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Can't open db :( : %s\n", sqlite3_errmsg(db));
         return 1;
     }
@@ -78,7 +397,7 @@ int main(){
     if(rc != SQLITE_OK) {
         fprintf(stderr, "SQL error: %s\n", errMsg);
         sqlite3_free(errMsg);
-    } else {printf("Table created!\n");} 
+    } else {printf("Table is ready!\n");} 
 
     // Networking stuff
     memset(&hints, 0, sizeof hints);
@@ -86,16 +405,59 @@ int main(){
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    rc = getaddrinfo(NULL, MYPORT, &hints, &res); // lidar com rc depois
+    status = getaddrinfo(NULL, MYPORT, &hints, &res); 
+    if (status != 0){
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        return 1;
+    }
 
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    rc = bind(sockfd, res->ai_addr, res->ai_addrlen); // lidar com rc depois
-    listen(sockfd, BACKLOG);
-    
-    addr_size = sizeof their_addr;
-    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+    status = bind(sockfd, res->ai_addr, res->ai_addrlen);
+    if (status != 0) {
+        fprintf(stderr, "bind error: %s\n", strerror(errno));
+        return 1;
+    }
 
-    // close database
+    status = listen(sockfd, BACKLOG);
+    if (status != 0) {
+        fprintf(stderr, "listen error: %s\n", strerror(errno));
+        return 1;
+    }
+    
+    printf("Server listening on port %s\n", MYPORT);
+
+    for (;;) {
+        addr_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+        if (new_fd == -1) {
+            fprintf(stderr, "accept error: %s\n", strerror(errno));
+            continue;
+        }
+
+        printf("Someone connect to host!\n");
+
+        int *client_fd_ptr = malloc(sizeof(int));
+        if (client_fd_ptr == NULL) {
+            fprintf(stderr, "malloc error\n");
+            close(new_fd);
+            continue;
+        }
+        *client_fd_ptr = new_fd;
+
+        // spawn thread for accepted connection
+        pthread_t thread;
+        status = pthread_create(&thread, NULL, client_handler, client_fd_ptr);
+        if (status != 0) {
+            fprintf(stderr, "pthread_create error\n");
+            free(client_fd_ptr);
+            close(new_fd);
+            continue;
+        }
+        pthread_detach(thread);
+    } 
+
+    // Cleanup
+    freeaddrinfo(res);
     sqlite3_close(db);
     return 0;
 }
